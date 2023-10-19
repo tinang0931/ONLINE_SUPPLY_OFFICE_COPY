f7123e5250b1ff6bfa48057b58bfa3b17fbac02d
ralpy11 <rlphtzn@gmail.com>
2023-10-19 00:28:04 -0700

    ralphii

--- accounts/models.py
+++ accounts/models.py
@@ -1,52 +1,4 @@
 from django.db import models
-from django.template import RequestContext
-
-
-class Requester (models.Model):
-    name = models.CharField(max_length=200,null=True)
-    email = models.CharField(max_length=200,null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self)   :
-        return self.name
-    
-class Tag (models.Model):
-    name = models.CharField(max_length=200,null=True)
-
-    def __str__(self)   :
-        return self.name
-
-class Products(models.Model):
-
-    CATEGORY = (
-    ('Sports', 'Sports'),
-    ('Laboratory', 'Laboratory')
-    )
-    name = models.CharField(max_length=200,null=True)
-    price = models.FloatField(null=True)
-    category = models.CharField(max_length=200,null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200,null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag)
-
-    def __str__(self)   :
-        return self.name
-
-
-class Status(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Approved', 'Approved'),
-        ('Declined', 'Declined'),
-    )
-
-    requester = models.ForeignKey(Requester, null=True, on_delete=models.SET_NULL)
-    products = models.ForeignKey(Products, null=True, on_delete=models.SET_NULL)
-    date_created = models.DateTimeField(auto_now_add=True)      
-    status = models.CharField(max_length=200,null=True, choices=STATUS)
-
-
-from django.db import models
 from django.contrib.auth.models import User 
 
 
@@ -86,25 +38,3 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
-class TransactionHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return f'Transaction by {self.user.username} at {self.timestamp}'
-    
-
-
-
-class VerificationCode(models.Model):
-    email = models.EmailField()
-    code = models.CharField(max_length=4)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'Code: {self.code} for {self.email}'
-
