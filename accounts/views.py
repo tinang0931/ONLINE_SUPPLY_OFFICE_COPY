103d56720ca8f7ca54025157db8f7ec16de3c606
gorgeousmee <lomogonjeanny@gmail.com>
2023-11-20 12:32:16 -0800

    11-20-2023

--- accounts/views.py
+++ accounts/views.py
@@ -368,8 +368,7 @@ def request(request):
         return redirect('requester')
 
     else:
-        csv_file_path ='C:/Users/cardosa.kristineanne/Desktop/INVENTORY/ONLINE_SUPPLY_OFFICE_COPY/items.csv'
-
+        csv_file_path ='C:/Users/lomogon.rohelyjean/Desktop/SUPPLY SYSTEM/items.csv'
         with open(csv_file_path, 'r') as file:
             reader = csv.DictReader(file)
             csv_data = list(reader)
