6b08dceab9e749c9cc49a69b6bb597010bab463d
kristina0331 <krisyang0931@gmail.com>
2024-01-09 09:13:39 +0800

    kristine/01-09-2024

--- accounts/views.py
+++ accounts/views.py
@@ -435,11 +435,6 @@ def cdpurchase(request):
 
 
 @authenticated_user
-def cdhome(request):
-    return render(request, 'accounts/Admin/Campus_Director/cdhome.html')
-
-
-@authenticated_user
 def cdresolution(request):
     return render(request, 'accounts/Admin/Campus_Director/cdresolution.html')
 
@@ -909,6 +904,118 @@ class PreqForm_boView(View):
                 checkout.save()
 
                 return redirect(reverse('preqform_bo', kwargs={'pr_id': pr_id}))
+            except Checkout.DoesNotExist:
+                return HttpResponse("Checkout not found.")
+            except Exception as e:
+                print(f"Error: {e}")
+                return HttpResponse("An error occurred while processing the form.")
+        else:
+            return HttpResponse("PR ID or new status not found in the form data.")
+
+def update_checkout_status(request, pr_id, new_status):
+    if request.method == 'POST':
+        try:
+            # Convert new_status to boolean
+            new_status = new_status.lower() == 'true' if isinstance(new_status, str) else new_status
+
+            # Update the status of the checkout directly
+            checkout = get_object_or_404(Checkout, pr_id=pr_id)
+            checkout.status_update_date = timezone.now()
+            checkout.is_approve = new_status
+            checkout.is_disapprove = not new_status
+            checkout.is_seen = True  # Mark as seen when the status is updated
+            checkout.save()
+
+            # Redirect after processing
+            return redirect(reverse('preqform_bo', kwargs={'pr_id': pr_id}))
+        except Checkout.DoesNotExist:
+            return HttpResponse("Checkout not found.")
+        except Exception as e:
+            # Handle exceptions, log errors, etc.
+            print(f"Error: {e}")
+            return HttpResponse("An error occurred while processing the form.")
+    else:
+        return HttpResponse("Invalid request method.")
+
+def cdpurchase(request):
+    checkouts = Checkout.objects.select_related('user').all()
+    comments = Comment.objects.all()
+
+    checkout_data_dict = {}
+
+    for checkout in checkouts:
+        pr_id = checkout.pr_id
+
+        latest_comment = comments.filter(pr_id=pr_id).order_by('-timestamp').first()
+
+        if pr_id not in checkout_data_dict:
+            checkout_data_dict[pr_id] = {
+                'pr_id': pr_id,
+                'first_name': checkout.user.first_name,
+                'last_name': checkout.user.last_name,
+                'submission_date': checkout.submission_date,
+                'purpose': checkout.purpose,
+                'is_approve': checkout.is_approve,
+                'is_disapprove': checkout.is_disapprove,
+                'is_seen': checkout.is_seen,  
+                'comment': latest_comment.content if latest_comment else "",
+                'status_update_date': latest_comment.timestamp if latest_comment else None,
+                 
+            }
+        else:
+            checkout_data_dict[pr_id]['purpose'] += f", {checkout.purpose}"
+           
+            checkout_data_dict[pr_id]['status_update_date'] = latest_comment.timestamp if latest_comment else None
+    checkout_data = list(checkout_data_dict.values())
+
+    return render(request, 'accounts/Admin/Campus_Director/cdpurchase.html', {'checkouts': checkout_data})
+
+class PreqForm_cdView(View):
+    template_name = 'accounts/Admin/Campus_Director/preqform_cd.html'
+
+    def get(self, request, pr_id):
+        # Use the pr_id to retrieve the corresponding Checkout object
+        checkout = get_object_or_404(Checkout, pr_id=pr_id)
+
+        # Get checkout items associated with the checkout
+        checkout_items = CheckoutItems.objects.filter(checkout=checkout)
+
+        context = {
+            'checkout_items': checkout_items,
+            'pr_id': pr_id,
+            'user': checkout.user,
+            'purpose': checkout.purpose,
+            'pending': not checkout.is_approve and not checkout.is_disapprove,
+            'approved': checkout.is_approve,
+            'disapproved': checkout.is_disapprove,
+            'is_seen': checkout.is_seen,
+            
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, pr_id):
+        new_status = request.POST.get('new_status')
+
+        if pr_id and new_status:
+            try:
+                checkout = Checkout.objects.get(pr_id=pr_id)
+
+                checkout.date_updated = timezone.now()
+                checkout.is_seen = True
+
+                # Set is_approve and is_disapprove to False initially
+                checkout.is_approve = False
+                checkout.is_disapprove = False
+
+                if new_status.lower() == 'true':
+                    checkout.is_approve = True
+                else:
+                    checkout.is_disapprove = True
+
+                checkout.save()
+
+                return redirect(reverse('preqform_cd', kwargs={'pr_id': pr_id}))
             except Checkout.DoesNotExist:
                 return HttpResponse("Checkout not found.")
             except Exception as e:
