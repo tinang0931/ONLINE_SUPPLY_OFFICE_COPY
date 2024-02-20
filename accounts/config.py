1cbb5747925b8b7504259b2707851ccd552ecb78
gwapocjhake <128198476+gwapocjhake@users.noreply.github.com>
2024-02-20 08:41:54 +0800

    02/12/24

--- accounts/config.py
+++ accounts/config.py
@@ -1,4 +1,4 @@
-CAMPUS_NAME = "Argao"
+CAMPUS_NAME = "boang mn deay"
 SITE_TITLE = " CTU-AC Supply Office Request Monitoring System"
 HEADING_TEXT = "Supply Office Request Monitoring System"
 SUBHEADING_TEXT = "Cebu Technological University - Argao Campus"
