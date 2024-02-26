ff0850f75563993d4eddf7759acd120184d770d6
Jekyangg <127277362+Jekyangg@users.noreply.github.com>
2024-02-26 11:33:01 +0800

    2/26/24

--- accounts/views.py
+++ accounts/views.py
@@ -137,26 +137,7 @@ def ppmp101(request):
 
     return render(request, 'accounts/User/ppmp101.html', context)
 
-def ppmpform(request, year, pr_id):
- 
-    
-    approved_checkouts = Checkout.objects.filter(bo_status='approved', cd_status='approved', user=request.user, year=year, pr_id=pr_id)
 
-
-    approved_items = CheckoutItems.objects.filter(checkout__in=approved_checkouts)
-
-
-    context = {
-        'approved_items': approved_items,
-        'year': year,
-        'pr_id': pr_id,
-        'bo_comment': approved_checkouts.first().bo_comment,
-        'cd_comment': approved_checkouts.first().cd_comment,
-        'SITE_TITLE': SITE_TITLE,
-        'CAMPUS_NAME': CAMPUS_NAME,
-    }
-
-    return render(request, 'accounts/User/myppmp.html', context)
 
 
 def landing(request):
@@ -285,34 +266,6 @@ def bac_request(request):
     }
     return render(request, 'accounts/Admin/BAC_Secretariat/bac_request.html', context)
 
-
-
-
-
-@regular_user_required
-def ppmp101(request):
-  
-    checkouts = Checkout.objects.filter(bo_status='approved', cd_status='approved', user=request.user)
-
-    checkout_data = []
-
-    for checkout in checkouts:
-        checkout_dict = {
-            'year': checkout.year,
-            'pr_id': checkout.pr_id,
-            'user': checkout.user,
-            'submission_date': checkout.submission_date,
-        }
-        checkout_data.append(checkout_dict)
-
-    context = {
-        'checkouts': checkout_data,
-        'user': request.user,
-        'SITE_TITLE' : SITE_TITLE,
-        'CAMPUS_NAME' : CAMPUS_NAME
-    }
-
-    return render(request, 'accounts/User/ppmp101.html', context)
 
 def ppmpform(request, year, pr_id):
  
@@ -935,6 +888,7 @@ def ppmp(request):
         )
         
         items = request.POST.getlist('item')
+        
         item_brands = request.POST.getlist('item_brand')
         units = request.POST.getlist('unit')
         estimate_budgets = request.POST.getlist('estimate_budget')
@@ -1340,14 +1294,15 @@ def preqform_bo(request, pr_id):
        
     elif request.method == 'GET':
         checkouts = get_object_or_404(Checkout, pr_id=pr_id)
-        checkout_items = CheckoutItems.objects.filter(checkout=checkouts)
+        checkout_item = CheckoutItems.objects.filter(checkout=checkouts)
         context = {
             'checkouts': checkouts,
-            'checkout_items': checkout_items,
+            'checkout_item': checkout_item,
             'pr_id': pr_id,
             'SITE_TITLE': SITE_TITLE,
             'CAMPUS_NAME': CAMPUS_NAME,
         }
+        
         return render(request, 'accounts/Admin/Budget_Officer/preqform_bo.html', context)
 
         
